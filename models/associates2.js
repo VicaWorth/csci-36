20c3f5eeb64bfbe823d6bfec248a30c70ecf93f5
Ben Lazzeroni <85390423+benlazzero@users.noreply.github.com>
2022-12-08 20:16:47 -0800

    Update associates2.js

--- models/associates2.js
+++ models/associates2.js
@@ -2,7 +2,7 @@ import { courses, programs, courprog } from '../database/sequelize.js'
 
 const associations = () => {
   programs.belongsToMany(courses, { through: { model: courprog, unique: false}});
-  courses.belongsToMany(programs, { through: { model: courprog, unqiue: false}});
+  courses.belongsToMany(programs, { through: { model: courprog, unique: false}});
 }
 
 export default associations;
