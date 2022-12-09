10c0dfe5d9c9eb69ffabca620801875377683095
benlazzero <85390423+benlazzero@users.noreply.github.com>
2022-12-08 20:11:24 -0800

    fixes sequelise dupe key error

--- models/associates2.js
+++ models/associates2.js
@@ -1,8 +1,8 @@
 import { courses, programs, courprog } from '../database/sequelize.js'
 
 const associations = () => {
-  programs.belongsToMany(courses, { through: courprog});
-  courses.belongsToMany(programs, { through: courprog});
+  programs.belongsToMany(courses, { through: { model: courprog, unique: false}});
+  courses.belongsToMany(programs, { through: { model: courprog, unqiue: false}});
 }
 
 export default associations;
